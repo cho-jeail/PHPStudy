52500bfa002493f015162de3620bf641e6721078
조재일 <winjl33@naver.com>
2021-09-16 12:20:20 +0900

    COOKIE 사용 관련 파일 생성 cookieTest.php 파일 생성  - 새로고침 시 count 변수 1씩 증가하는 코드 index.php <a>태그 링크 추가 2021-09-16, 12:20 조재일

--- index.php
+++ index.php
@@ -48,5 +48,6 @@
         <div><a href="filterTest.php">29. PHP Filters 사용</a></div>
         <div><a href="fileUploadTest.html">30. 파일 업로드</a></div>
         <div><a href="GD1.html">31. GD 라이브러리 활용 1</a></div>
+        <div><a href="cookieTest.php">32.COOKIE</a></div>
     </body>
 </html>
